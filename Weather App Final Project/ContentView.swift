8ea7833cd78f9f9becdd124a15ae23c0c598c8cf
Ahmed Wardhere <ward0296@algonquinlive.com>
2024-11-25 20:13:13 -0500

    Initial Commit

---
+++ Weather App Final Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather App Final Project
+//
+//  Created by Ahmed  on 2024-11-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather App Final Project/Weather_App_Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_Final_ProjectApp.swift
+//  Weather App Final Project
+//
+//  Created by Ahmed  on 2024-11-25.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App_Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
