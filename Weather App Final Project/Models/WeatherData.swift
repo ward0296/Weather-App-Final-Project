f9575b11991a9f6898a9885d6185c83d035b3896
ward0296 <ward0296@algonquinlive.com>
2024-12-06 22:58:55 -0500

    weather data comments

--- Weather App Final Project/Models/WeatherData.swift
+++ Weather App Final Project/Models/WeatherData.swift
@@ -9,27 +9,27 @@ import SwiftUI
 
 // Weather Response Model
 struct WeatherResponse: Codable {
-    let name: String
+    let name: String           // City name
     let sys: Sys               
-    let weather: [Weather]
-    let main: Main
-    let wind: Wind
-    let timezone: Int
+    let weather: [Weather]     // Weather conditions
+    let main: Main             // Main weather details
+    let wind: Wind             // Wind data
+    let timezone: Int          // Timezone UTC
 }
 
 struct Sys: Codable {
-    let country: String       
+    let country: String        // Country code (e.g., "US")
 }
 
 struct Weather: Codable {
-    let main: String
-    let description: String
+    let main: String           // General weather condition (example: "Clear")
+    let description: String    // Weather description (example: "clear sky")
     let icon: String
 }
 
 struct Main: Codable {
-    let temp: Double
-    let humidity: Int
+    let temp: Double           // Temperature in Celsius
+    let humidity: Int          // Humidity percentage
 }
 
 struct Wind: Codable {
